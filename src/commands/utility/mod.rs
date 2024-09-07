f26fd6f0a2d3a2844881cb9f1b11ff0986b4c8ef
Ruben <rubenthetoad@gmail.com>
2024-09-07 21:42:21 +0200

    allow manually setting a fallback channel

--- src/commands/utility/mod.rs
+++ src/commands/utility/mod.rs
@@ -1,5 +1,9 @@
 mod set_utc_offset;
+mod set_fallback_channel;
 
-pub fn commands() -> [crate::Command; 1] {
-    [set_utc_offset::set_utc_offset()]
+pub fn commands() -> [crate::Command; 2] {
+    [
+        set_utc_offset::set_utc_offset(),
+        set_fallback_channel::set_fallback_channel()
+    ]
 }
---
+++ src/commands/utility/set_fallback_channel.rs
@@ -0,0 +1,52 @@
+use poise::CreateReply;
+use crate::commands::util::{get_internal_channel_id};
+use crate::{BOT_COLOR, Context, Error};
+use poise::serenity_prelude::{ChannelId, ChannelType, CreateEmbed, CreateEmbedAuthor};
+use sqlx::query;
+
+/// Set the server's fallback channel
+///
+/// Example: h!setfallback CHANNELID
+#[poise::command(
+    slash_command,
+    prefix_command,
+    rename = "setfallback",
+    aliases("fallbackchannel", "setfallbackchannel"),
+    discard_spare_arguments,
+    required_permissions = "MANAGE_CHANNELS", // remove for a slash-command only bot
+    default_member_permissions = "MANAGE_CHANNELS", // this currently only hides the command from the command picker, perms are handled by above
+    guild_only
+)]
+pub async fn set_fallback_channel(
+    ctx: Context<'_>,
+    #[description = "Channel to be used when other options are unavailable"]
+    #[channel_types("Text")]
+    channel: Option<ChannelId>,
+) -> Result<(), Error> {
+    let channel = match channel {
+        Some(channel) => channel,
+        None => ctx.channel_id(),
+    };
+    let Some(guild_channel) = channel.to_channel(ctx).await?.guild() else {
+        return Err("something really weird happened and the guild-only command returned a channel that's not in a guild".into());
+    };
+    if guild_channel.kind != ChannelType::Text {
+        return Err("Please specify a text channel.".into());
+    }
+    
+    let i_channel_id = get_internal_channel_id(ctx.data(), channel).await?;
+    let guild_id = guild_channel.guild_id.get() as i64;
+    query!(r"UPDATE guilds SET fallback_channel = ? WHERE discord_id = ?", i_channel_id, guild_id)
+        .execute(&ctx.data().pool)
+        .await?;
+    
+    let embed = CreateEmbed::new()
+        .author(CreateEmbedAuthor::from(ctx.author().clone()))
+        .color(BOT_COLOR)
+        .title("Fallback channel updated.")
+        .description(format!(
+            "I will now use <#{}> as fallback channel.", channel.get()
+        ));
+    ctx.send(CreateReply::default().embed(embed)).await?;
+    Ok(())
+}
