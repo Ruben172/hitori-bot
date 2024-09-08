017508e43354380c7e0c6a2222cf2b3d805274aa
Ruben <rubenthetoad@gmail.com>
2024-09-08 17:28:22 +0200

    improve unfollow embed

--- src/commands/reminders/unfollow.rs
+++ src/commands/reminders/unfollow.rs
@@ -1,9 +1,11 @@
-use crate::commands::reminders::util::{cache_reminder, get_next_reminder_ts, reminder_exists_and_active, user_ids_from_reminder_id};
-use crate::{BOT_COLOR, Context, Error};
-use poise::serenity_prelude::CreateEmbed;
+use crate::commands::reminders::util::{
+    cache_reminder, get_next_reminder_ts, reminder_exists_and_active, user_ids_from_reminder_id,
+};
+use crate::commands::util::get_internal_user_id;
+use crate::{Context, Error, BOT_COLOR};
+use poise::serenity_prelude::{CreateEmbed, CreateEmbedAuthor};
 use poise::CreateReply;
 use sqlx::query;
-use crate::commands::util::get_internal_user_id;
 
 /// Unfollow or remove a reminder
 ///
@@ -19,16 +21,17 @@ pub async fn unfollow(
 ) -> Result<(), Error> {
     let reminder_id = reminder_id as i64;
     if !reminder_exists_and_active(ctx.data(), reminder_id).await {
-        return Err("Uh, it seems the reminder doesn't exist or it's already expired... S-sorry, but I can't remove you from it.".into()); // TODO: maybe get a better one for this?
+        return Err("Uh, it seems the reminder doesn't exist or it's already expired... S-sorry, but I can't remove you from it.".into());
+        // TODO: maybe get a better one for this?
     }
     let user_ids = user_ids_from_reminder_id(ctx.data(), reminder_id).await?;
 
     let user_id = ctx.author().id;
     if !user_ids.contains(&user_id) {
-        return Err("Um, it looks like you're not following this reminder... S-sorry, but I can't remove you from it.".into());        
+        return Err("Um, it looks like you're not following this reminder... S-sorry, but I can't remove you from it.".into());
     }
 
-    let title: String;
+    let description: String;
     let ephemeral: bool;
     let i_user_id = get_internal_user_id(ctx.data(), user_id).await?;
     query!(
@@ -39,7 +42,7 @@ pub async fn unfollow(
     .execute(&ctx.data().pool)
     .await?;
     if user_ids.len() > 1 {
-        title = format!("O-okay, you'll no longer be notified for reminder #{reminder_id}. I-I hope that's alright!");
+        description = format!("O-okay, you'll no longer be notified for reminder #{reminder_id}. I-I hope that's alright!");
         ephemeral = true;
     } else {
         query!("UPDATE reminders SET active = 0 WHERE id = ?", reminder_id)
@@ -52,11 +55,14 @@ pub async fn unfollow(
         if let Some(reminder) = get_next_reminder_ts(&ctx.data().pool).await {
             cache_reminder(ctx.data(), reminder); // Populate the cached reminder again
         }
-        title = format!("Um, reminder #{reminder_id} has been removed. S-since you were the only one tracking it, it... um, no longer exists. I-I hope that's okay!");
+        description = format!("Um, reminder #{reminder_id} has been removed. S-since you were the only one tracking it, it... um, no longer exists. I-I hope that's okay!");
         ephemeral = false;
     }
 
-    let embed = CreateEmbed::new().title(title).color(BOT_COLOR);
+    let embed = CreateEmbed::new()
+        .author(CreateEmbedAuthor::from(ctx.author().clone()))
+        .description(description)
+        .color(BOT_COLOR);
     ctx.send(CreateReply::default().embed(embed).ephemeral(ephemeral)).await?;
 
     Ok(())
